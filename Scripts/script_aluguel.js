8efed4c09709c4eb0919d25cda3641ea373b8dba
AlessandroSilvaCard <alesilvacardoso01@icloud.com>
2025-05-15 18:42:08 -0300

    Update script_aluguel.js

--- Scripts/script_aluguel.js
+++ Scripts/script_aluguel.js
@@ -1,3 +1,22 @@
+//pop up de log
+function toggleDropdown() {
+        const dropdown = document.getElementById('user-dropdown');
+        dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+    }
+
+function logout() {
+                // Redirecione para a página de login ou realize outra ação
+        window.location.href = '../index.html';
+    }
+
+    // Fecha o dropdown ao clicar fora dele
+    document.addEventListener('click', function (event) {
+        const dropdown = document.getElementById('user-dropdown');
+        const userIcon = document.querySelector('.img_nav_login');
+        if (!dropdown.contains(event.target) && event.target !== userIcon) {
+            dropdown.style.display = 'none';
+        }
+    }); 
 document.getElementById('reservation-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -69,3 +88,15 @@ function updateLocationInfo() {
 
         calendar.render();
     });
+function selectTime(button) {
+    // Remove a classe 'selected' de todos os botões
+    const buttons = document.querySelectorAll('.time-button');
+    buttons.forEach(btn => btn.classList.remove('selected'));
+
+    // Adiciona a classe 'selected' ao botão clicado
+    button.classList.add('selected');
+
+    // Define o valor do horário no campo oculto
+    const timeInput = document.getElementById('time');
+    timeInput.value = button.getAttribute('data-time');
+}
